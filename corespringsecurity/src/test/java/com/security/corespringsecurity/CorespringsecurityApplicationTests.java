c0c7cd35f3855f453ceba58c4e5d28d118cef0b6
김민정 <minjeong@gimminjeong-ui-MacBookAir.local>
2023-03-22 00:33:18 +0900

    project setting

---
+++ corespringsecurity/src/test/java/com/security/corespringsecurity/CorespringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.corespringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorespringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
