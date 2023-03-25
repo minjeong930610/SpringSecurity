c0c3ddad0da4f3a324a766e43d42b723dda7b020
김민정 <minjeong@gimminjeong-ui-MacBookAir.local>
2023-03-26 06:18:41 +0900

    CustomAuthenticationProvider

--- corespringsecurity/src/main/java/com/security/corespringsecurity/security/service/CustomUserDetailsService.java
+++ corespringsecurity/src/main/java/com/security/corespringsecurity/security/service/CustomUserDetailsService.java
@@ -18,6 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+
         Account account = userRepository.findByUsername(username);
 
         if(account == null){
